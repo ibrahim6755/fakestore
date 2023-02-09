2dabe3d6743a72dcf5d98eeb5b19092e8ed50b4a
ibrahim6755 <ibrahimghani020@gmail.com>
2023-02-09 09:44:53 +0000

    changes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -15,27 +15,30 @@ function Main() {
     }, [])
 
     return (
-        <div className="row">
+        <>
             <div className="div">
-                <h1 className='text-center p-3 text-warning bg-dark'>SAMPLE STORE</h1>
+                <h1 className='text-center p-3 text-warning  shadow bg-body-tertiary rounded mt-3'>SAMPLE STORE</h1>
             </div>
-            {
-                products.map((product, index) => {
-                    return <div key={index} className='col-md-3 p-2'>
-                        <div className="card p-4" >
-                <img src={product.image} className="card-img-top p-3" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title ">{product.title}</h5>
-                    <p className="card-text fs-6">{product.description}</p>
-                    <a href="#" className="btn btn-primary">{product.price}</a>
-                </div>
-            </div>
-                    </div>
-                })
-            }
-           
+            <div className="row px-4 mt-4">
 
-        </div>
+                {
+                    products.map((product, index) => {
+                        return <div key={index} className='col-md-3 p-2 px-2'>
+                            <div className="card px-2" >
+                                <img src={product.image} className="card-img-top px-5 pt-3" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title ">{product.title}</h5>
+                                    <p className="card-text fs-6 lh-base font-monospace">{product.description}</p>
+                                    <a href="#" className="btn btn-primary"><span className=' fw-bold'>Price:</span> {product.price}$</a>
+                                </div>
+                            </div>
+                        </div>
+                    })
+                }
+
+
+            </div>
+        </>
     )
 }
 
